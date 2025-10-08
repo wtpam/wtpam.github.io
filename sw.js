25a79e48e5b1f9ef5d42eed14fd7a4fdb67d554a
wtpam <45529461+wtpam@users.noreply.github.com>
2025-10-08 11:15:49 +0800

    從此開始sweetalert2(加大popup、校車問與答pdf加/edit置底、每小時更新、開學課表、scrollIntoView～聯絡簿) 改單張行事曆、平時成績、自然重點、反霸凌專線、select姓名中用Ｏ、班費、社會重點、社會解析、班親會(家長日)、Google Classroom、數位學生證、段一、PWA

--- sw.js
+++ sw.js
@@ -2,3 +2,7 @@
 self.addEventListener('install', e => {
   console.log('Service Worker installed');
 });
+
+self.addEventListener('fetch', event => {
+  console.log('攔截請求:', event.request.url);
+});
